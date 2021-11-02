da9f9405e4f0fc0587d1cb7c3c870b4e2f669e78
tharinduug <32195143+tharinduug@users.noreply.github.com>
2021-11-02 21:16:55 +0300

    Changes for TF

---
+++ AKS/CreateAKS-variables.tf
@@ -0,0 +1,65 @@
+variable "prefix" {
+  description = "Project Code or Name"
+  type        = string
+  default = "csc-mon"
+}
+
+variable env {
+  description = "Environment where VMs will be provisioned"
+  type        = string
+  default     = "dev"
+}
+
+variable region {
+  description = "Management Console Region"
+  type        = string
+  default     = "eu"
+}
+
+variable location {
+  description = "Azure Region"
+  type        = string
+  default     = "West Europe"
+}
+
+variable "subscription_id" {
+  description = "Subscription ID"
+  type        = string
+  default = "subscription_id"
+}
+
+variable "client_id" {
+  description = "Client ID"
+  type        = string
+  default = "client_id"
+}
+
+variable "client_secret" {
+  description = "Client Secret"
+  type        = string
+  default = "client_secret"
+}
+
+variable "tenant_id" {
+  description = "Tenant ID"
+  type        = string
+  default = "tenant_id"
+}
+
+variable "rg" {
+  description = "Resource Group"
+  type        = string
+  default = "csc-proj-tcs-moni-poc-rg"
+}
+
+variable "lb_subnet" {
+  description = "Subnet"
+  type        = string
+  default = "promdev"
+}
+
+variable "vnet" {
+  description = "Virtual Network"
+  type        = string
+  default = "csc-proj-tcs-moni-poc-euwe-vnet"
+}

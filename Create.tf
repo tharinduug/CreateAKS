9bb407ec61dd50421102e8e9220ee2f577b2a8eb
tharinduug <32195143+tharinduug@users.noreply.github.com>
2021-11-04 12:12:32 +0300

    Update Create.tf

--- Create.tf
+++ Create.tf
@@ -1,7 +1,11 @@
 ######################## CONFIGURE MS AZURE PROVIDER ############################
 
 provider "azurerm" {
-  version = "=2.0.0"
+  subscription_id = var.azure_subscription_id
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_client_secret
+  tenant_id       = var.azure_tenant_id
+  version         = "=1.36.0"
   features {}
 }
 

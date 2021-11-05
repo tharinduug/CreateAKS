e304a6d3eed878cc0e3e22da2bbc99844dd4ac47
tharinduug <32195143+tharinduug@users.noreply.github.com>
2021-11-05 21:50:41 +0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 resource "local_file" "kubeconfig" {
-  depends_on   = [azurerm_kubernetes_cluster.cluster]
+  depends_on   = [azurerm_kubernetes_cluster.k8s]
   filename     = "kubeconfig"
-  content      = azurerm_kubernetes_cluster.cluster.kube_config_raw
+  content      = azurerm_kubernetes_cluster.k8s.kube_config_raw
 }

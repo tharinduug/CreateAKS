7bfb0e7ca8b53196ba7385b522eb40bc8d2b357d
tharinduug <32195143+tharinduug@users.noreply.github.com>
2021-11-07 11:42:26 +0300

    Update Create.tf

--- Create.tf
+++ Create.tf
@@ -53,7 +53,7 @@ resource "azurerm_subnet" "subnet" {
   name                 = "${(var.prefix)}-subnet"
   virtual_network_name = azurerm_virtual_network.example.name
   resource_group_name  = azurerm_resource_group.AFS-AKS.name
-  address_prefixes     = ["10.0.0.0/16"]
+  address_prefixes     = ["10.1.0.0/16"]
 }
 
 

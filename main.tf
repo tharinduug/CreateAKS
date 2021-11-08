c9dfc774b7ca0b26b66e36370e1fa79a8b2e9863
tharinduug <32195143+tharinduug@users.noreply.github.com>
2021-11-08 13:26:08 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 ######################## CONFIGURE MS AZURE PROVIDER ############################
 
 provider "azurerm" {
-  subscription_id = var.azure_subscription_id
-  client_id       = var.azure_client_id
-  client_secret   = var.azure_client_secret
-  tenant_id       = var.azure_tenant_id
+  #subscription_id = var.azure_subscription_id
+  #client_id       = var.azure_client_id
+  #client_secret   = var.azure_client_secret
+  #tenant_id       = var.azure_tenant_id
   features {}
 }
 
